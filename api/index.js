ec20a02e2396ad45ede2ad0d773741a304a786ce
OrdChaos <orderchaos@ordchaos.com>
2024-08-14 16:22:43 +0800

    cors

--- api/index.js
+++ api/index.js
@@ -2,29 +2,7 @@ const mysql = require('mysql2/promise');
 const axios = require('axios');
 require('dotenv').config();
 
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-    if (req.method === 'OPTIONS') {
-      res.status(200).end();
-      return;
-    }
-    return await fn(req, res);
-}
-  
-const handler = (req, res) => {
-    const d = new Date();
-    res.end(d.toString());
-}
-
 module.exports = async (req, res) => {
-    allowCors(handler);
-
     const apiKey = process.env.API_KEY;
     const apiUrl = "https://dashscope.aliyuncs.com/compatible-mode/v1";
     const content = req.query.content;
